edd8018afe99fc1269a1484fd9bfa9978e02e61c
Paulin Trognon <paulin.trognon@gmail.com>
2015-03-30 23:50:23 +0200

    board now looking good

--- main.js
+++ main.js
@@ -4,4 +4,13 @@ if (Meteor.isClient) {
   Template.board.helpers({
     squares: Squares.find({})
   });
+
+  Template.square.helpers({
+    getX: function () {
+      return this.pos.x * 50;
+    },
+    getY: function () {
+      return this.pos.y * 50;
+    }
+  });
 }
